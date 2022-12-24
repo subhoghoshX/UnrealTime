71f005c0bf54c103b4a42d17724e0f3e1ea88729
Subho <contact.subhoghosh@gmail.com>
2022-12-24 20:49:36 +0530

    fix: build error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const next = require("next");
-const http = require("node:http");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
