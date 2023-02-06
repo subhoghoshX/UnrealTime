294f8f628302470ac0330e1e1c8dc6a6816643cd
Subho <contact.subhoghosh@gmail.com>
2023-02-06 23:10:59 +0530

    initial change

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,17 +1,24 @@
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { Socket } from "socket.io-client";
+import getNow from "../utils/getNow";
 
 interface Props {
   socket: Socket;
 }
 
+interface Message {
+  text: string;
+  senderId: string;
+}
+
 export default function Chat({ socket }: Props) {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [messageText, setMessageText] = useState("");
 
   useEffect(() => {
-    socket.on("chat-message", (arg) => {
-      setMessages((messages) => [...messages, arg]);
+    socket.on("chat-message", (message) => {
+      setMessages((messages) => [...messages, message]);
     });
 
     return () => {
@@ -20,28 +27,55 @@ export default function Chat({ socket }: Props) {
   }, []);
 
   return (
-    <div className="flex h-full flex-col border-l border-white">
-      <ul className="flex-grow overflow-auto p-5 text-white">
-        {messages.map((message, i) => (
-          <li key={i}>{message}</li>
-        ))}
-      </ul>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          socket.emit("chat-message", message);
-          setMessage("");
-          setMessages((messages) => [...messages, message]);
-        }}
-      >
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="border"
-        />
-        <button className="bg-blue-500 px-4 py-2">Send</button>
-      </form>
+    <div className="h-full p-4">
+      <div className="flex h-full w-80 flex-col rounded-md bg-white p-3">
+        <ul className="flex-grow space-y-3 overflow-auto p-5">
+          {messages.map(({ text, senderId }, i) => (
+            <li
+              key={i}
+              className={clsx("flex flex-col", {
+                "items-end": socket.id === senderId,
+                "items-start": socket.id !== senderId,
+              })}
+            >
+              <p
+                className={clsx("rounded-xl px-3 py-1", {
+                  "rounded-bl-none bg-zinc-200": socket.id !== senderId,
+                  "rounded-br-none bg-orange-200": socket.id === senderId,
+                })}
+              >
+                {text}
+              </p>
+              <p className="text-xs text-zinc-700">
+                <span className="">Mayank</span>
+              </p>
+            </li>
+          ))}
+        </ul>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            socket.emit("chat-message", {
+              text: messageText,
+              senderId: socket.id,
+            });
+            setMessageText("");
+            setMessages((messages) => [
+              ...messages,
+              { text: messageText, senderId: socket.id },
+            ]);
+          }}
+          className="flex shrink-0 overflow-hidden rounded-lg"
+        >
+          <input
+            type="text"
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+            className="h-full rounded-l-lg border border-r-0 px-3 py-1 focus:outline-none"
+          />
+          <button className="bg-blue-500 px-4 py-2">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
